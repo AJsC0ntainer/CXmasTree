8e141627b56bb77492924f29448f72031b34c7bc
Argenis J. Cunha <argenisjc@hotmail.com>
2025-03-19 14:01:15 -0500

    Commi from VSCode

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
       // Outer loop.
-      for (int i = 1; i <= 5; i++) {
-        //System.out.print("*"); // Executes 2 times
-        
+      for (int i = 1; i <= 5; i++) { //loop for 5 lines
+        //i: change rom 1,2,3...
         // Inner loop
-        for (int j = 1; j <= i; j++) {
-          System.out.print("*"); // Executes 6 times (2 * 3)
+        for (int j = 1; j <= i; j++) { //prin start per line
+          System.out.print("*"); //prin the start
         }
-        System.out.println();
+        System.out.println();//line space
       } 
     }
   }
